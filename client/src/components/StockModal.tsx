2bbfdf14b874ff45e7ca635ecbc19d4f6531bed0
M@x <vmaxbh@gmail.com>
2026-06-29 07:58:47 -0300

    correção modal estoque

--- client/src/components/StockModal.tsx
+++ client/src/components/StockModal.tsx
@@ -215,7 +215,7 @@ export function StockModal({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="stock-dialog">
         <DialogHeader>
           <DialogTitle>
             {editId ? "Editar Movimentacao" : "Nova Movimentacao de Estoque"}
